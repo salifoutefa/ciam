603d548f5592cb121eeefb2fe4fb9bd024af964c
tsanga_f <tsanga_f@LPT-62CP2>
2022-09-08 16:09:09 +0100

    Add project files.

---
+++ locales/strings.js
@@ -0,0 +1,17 @@
+var STR_VERIFY_NUMBER_ENTER_OTP_FR = "Vérifiez votre numéro et entrez OTP";
+var STR_ENTER_PHONE_NUMBER_OTP_FR = "Veuillez entrer votre numéro de téléphone pour recevoir un";
+var STR_OTP_ENTER_RECEIVED_OTP_FR = "OTP (One Time Pin) et entrez l'OTP reçu";
+var STR_VERIFY_FR = "Vérifier";
+var STR_PHONE_NUM_FR = "Numéro de téléphone";
+var STR_ENTER_OTP_FR = "Entrez OTP";
+var STR_SIGN_IN_FR = "S'identifier";
+var STR_RESEND_OTP_FR = "Renvoyer OTP";
+
+var STR_VERIFY_NUMBER_ENTER_OTP_EN = "Verify your number and enter OTP";
+var STR_ENTER_PHONE_NUMBER_OTP_EN = "Please enter your phone number to receive an";
+var STR_OTP_ENTER_RECEIVED_OTP_EN = "OTP (One Time Pin) and enter the received OTP";
+var STR_VERIFY_EN = "Verify";
+var STR_PHONE_NUM_EN = "Phone Number";
+var STR_ENTER_OTP_EN = "Enter OTP";
+var STR_SIGN_IN_EN = "Sign In";
+var STR_RESEND_OTP_EN = "Resend OTP";     
